babc1a7b4089520e3356fcda6ab6b01ab5e7d9f8
lucas556 <56923561@qq.com>
2018-04-28 14:34:05 +0800

    CONFIG

---
+++ config.js
@@ -0,0 +1,37 @@
+
+var _reqInterval = 500;
+
+var blockProducerList = [
+  {
+    "bp_name": "eosbeijing",
+    "organisation": "EOS Beijing",
+    "location": "hongkong",
+    "node_addr": "150.109.65.114",
+    "port_http": "8888",
+    "port_ssl": "",
+    "port_p2p": "9876",
+    "block_signing_key": "EOS6q3CV1Wv4nxwjw1No7M22CcM7AeRM2eZdaaNqsEmouD3saMArj"
+  },
+  {
+    "bp_name": "eosasia",
+    "organisation": "EOS Asia",
+    "location": "",
+    "node_addr": "bp-test.eosasia.one",
+    "port_http": "9893",
+    "port_ssl": "",
+    "port_p2p": "9892",
+    "block_signing_key": "EOS7P3hcQVQg4yMF77HiafzjR5RThZ3gqVzvVhAA7TQ8FJYhGqovV"
+  },
+
+    {
+    "bp_name": "eossv",
+    "organisation": "pacificrim.eossv.org	",
+    "location": "EOS Silicon Valley",
+    "node_addr": "pacificrim.eossv.org",
+    "port_http": "9893",
+    "port_ssl": "",
+    "port_p2p": "9892",
+    "block_signing_key": "EOS4uNq9sK26Vj7yTAhKt5UvcDdx5KS7Sioy3FFPhd6gafMWu7d5B"
+  },
+
+]
